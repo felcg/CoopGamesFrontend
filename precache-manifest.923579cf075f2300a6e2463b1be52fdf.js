8860b6820086519deadc32a929add521f3aedceb
felcg <xfelipecg@gmail.com>
2020-06-27 23:10:33 -0300

    Updates

---
+++ precache-manifest.923579cf075f2300a6e2463b1be52fdf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54c8d9e4b27872efe7ddf26ca1d5b9b",
+    "url": "/Gamelogged/index.html"
+  },
+  {
+    "revision": "582f6eb92b4ec764f3a6",
+    "url": "/Gamelogged/static/css/main.f9567403.chunk.css"
+  },
+  {
+    "revision": "697de82f7ea540425992",
+    "url": "/Gamelogged/static/js/2.5508beb0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Gamelogged/static/js/2.5508beb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582f6eb92b4ec764f3a6",
+    "url": "/Gamelogged/static/js/main.2dc818c8.chunk.js"
+  },
+  {
+    "revision": "2e23df4fafa7d1fda9b0",
+    "url": "/Gamelogged/static/js/runtime-main.ffd1e65e.js"
+  },
+  {
+    "revision": "c951b2133d5f03942ec7ce09f2020a71",
+    "url": "/Gamelogged/static/media/suikoden-2-box-art-crop_1280.0.0.c951b213.jpg"
+  }
+]);
