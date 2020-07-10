9867519c2f7a8648755053960881fd6a8f1b2e8a
felcg <xfelipecg@gmail.com>
2020-07-10 19:49:32 -0300

    Updates

---
+++ precache-manifest.aa571d52589385e1aea49f3717d0c695.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce28dc9924c8cc47989ec0214df1f07b",
+    "url": "/Gamelogged/index.html"
+  },
+  {
+    "revision": "d21f0a04e759e99b70c5",
+    "url": "/Gamelogged/static/css/main.9d1dff8e.chunk.css"
+  },
+  {
+    "revision": "8a7c3d4b6c89d3d09b66",
+    "url": "/Gamelogged/static/js/2.e89b7bab.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/Gamelogged/static/js/2.e89b7bab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d21f0a04e759e99b70c5",
+    "url": "/Gamelogged/static/js/main.5c8fcc4f.chunk.js"
+  },
+  {
+    "revision": "2e23df4fafa7d1fda9b0",
+    "url": "/Gamelogged/static/js/runtime-main.ffd1e65e.js"
+  },
+  {
+    "revision": "c951b2133d5f03942ec7ce09f2020a71",
+    "url": "/Gamelogged/static/media/suikoden-2-box-art-crop_1280.0.0.c951b213.jpg"
+  }
+]);
